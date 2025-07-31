9424c9d597ea91f62824aa09efa5dd986736d63b
Soham <sohampatel1117@gmail.com>
2025-07-30 22:58:08 -0500

    Added SLF4JBridgeHandler to ResumeUpdaterApplication

--- src/main/java/com/resume/ResumeUpdaterApplication.java
+++ src/main/java/com/resume/ResumeUpdaterApplication.java
@@ -1,5 +1,8 @@
 package com.resume;
 
+import java.util.logging.LogManager;
+
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ResumeUpdaterApplication {
 
     public static void main(String[] args) {
+        LogManager.getLogManager().reset();
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
         SpringApplication.run(ResumeUpdaterApplication.class, args);
     }
 } 

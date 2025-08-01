d044b2e846b68d1ce327b62ec41c126e488849c2
Soham <sohampatel1117@gmail.com>
2025-07-31 21:46:46 -0500

    Fixed logging issue

--- src/main/java/com/resume/ResumeUpdaterApplication.java
+++ src/main/java/com/resume/ResumeUpdaterApplication.java
@@ -1,18 +1,11 @@
 package com.resume;
 
-import java.util.logging.LogManager;
-
-import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ResumeUpdaterApplication {
-
     public static void main(String[] args) {
-        LogManager.getLogManager().reset();
-        SLF4JBridgeHandler.removeHandlersForRootLogger();
-        SLF4JBridgeHandler.install();
         SpringApplication.run(ResumeUpdaterApplication.class, args);
     }
-} 
+}
